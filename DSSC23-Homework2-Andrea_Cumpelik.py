433a110ee657a155503d9bc25ae7b9d32a17ee2c
Andrea <acumpelik@gmail.com>
2023-03-05 20:16:04 +0100

    mostly still playing around with the univariate gaussian for hw1, plotted raw trace for hw2

---
+++ DSSC23-Homework2-Andrea_Cumpelik.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Plot the voltage signal x(t) trace1 from the microelectrode
+array and visually examine it. Spikes are very fast downward voltage excursions
+(sometimes reaching to ~ -100 μV ), followed by a small overshoot (zoom in to
+a few spikes to see how they typically look like).
+
+To get some sense of the signal, plot a probability distribution function (properly
+normalized, so that the integral of dxP (x) = 1), of x(t). Estimate the error bars on the PDF
+by splitting the data multiple times into halves and compute the SD over PDF
+estimates constructed from halves of the data. Is there any obvious feature for
+negative voltages in the histogram where you could draw a threshold to recognize
+the spikes easily? To identify the spikes, you can set a threshold. Scan a range of
+thresholds, from 70 μV and 30 μV ; whenever the signal crosses the threshold
+in a downward direction (please pay attention to this definition!), identify a
+putative spike, and plot the number of spikes as a function of the threshold. By
+examining the trace in detail, can you claim that any specific threshold is a good
+choice for spike detection?
+"""
+%matplotlib auto
+
+import numpy as np
+import matplotlib.pyplot as plt
+trace1 = np.loadtxt(fname=('./trace1.txt')) # import the data
+
+# %%
+# Plot trace 1
+plt.plot(trace1)
+
+# Plot a subset
+plt.plot(trace1[80000:105000])
